03fe9f8e3b6d1051664f96256ba61a53f553a8b8
Necromos <masternecromos@gmail.com>
2013-04-19 08:11:15 +0200

    dorzucam to co mam + WIDOKI to folder z foundation

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,28 +1,91 @@
-CREATE TABLE tbl_user (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(128) NOT NULL,
-    password VARCHAR(128) NOT NULL,
-    email VARCHAR(128) NOT NULL
+CREATE TABLE user (
+  id_user INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nick VARCHAR(20) NOT NULL UNIQUE,
+  firstname VARCHAR(20),
+  lastname VARCHAR(20),
+  email VARCHAR(100) NOT NULL UNIQUE,
+  isadmin TINYINT(1) NOT NULL,
+  issectioneditor TINYINT(1) NOT NULL,
+  iseditor TINYINT(1) NOT NULL,
+  isreviewer TINYINT(1) NOT NULL,
+  ismanager TINYINT(1) NOT NULL,
+  register_date DATE
 );
 
-INSERT INTO tbl_user (username, password, email) VALUES ('test1', 'pass1', 'test1@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test2', 'pass2', 'test2@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test3', 'pass3', 'test3@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test4', 'pass4', 'test4@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test5', 'pass5', 'test5@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test6', 'pass6', 'test6@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test7', 'pass7', 'test7@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test8', 'pass8', 'test8@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test9', 'pass9', 'test9@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test10', 'pass10', 'test10@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test11', 'pass11', 'test11@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test12', 'pass12', 'test12@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test13', 'pass13', 'test13@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test14', 'pass14', 'test14@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test15', 'pass15', 'test15@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test16', 'pass16', 'test16@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test17', 'pass17', 'test17@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test18', 'pass18', 'test18@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test19', 'pass19', 'test19@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test20', 'pass20', 'test20@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test21', 'pass21', 'test21@example.com');
+CREATE TABLE article (
+  id_article INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  author INT NOT NULL,
+  title VARCHAR(50),
+  content LONGBLOB NOT NULL,
+  create_date DATE
+);
+
+ALTER TABLE article
+ADD CONSTRAINT FK_article
+FOREIGN KEY (author) REFERENCES user(id_user)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+CREATE TABLE status (
+  id_status INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20) NOT NULL UNIQUE  
+);
+
+CREATE TABLE article_history (
+  id_history INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  article INT NOT NULL,
+  status INT NOT NULL,
+  date DATE NOT NULL,
+  isenabled TINYINT(1) NOT NULL
+);
+
+ALTER TABLE article_history
+ADD CONSTRAINT FK_article_status_article
+FOREIGN KEY (article) REFERENCES article(id_article)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+ALTER TABLE article_history
+ADD CONSTRAINT FK_article_status_status
+FOREIGN KEY (status) REFERENCES status(id_status)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+CREATE TABLE tag (
+  id_tag INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20)
+);
+
+CREATE TABLE article_tag (
+  article INT NOT NULL,
+  tag INT NOT NULL
+);
+
+ALTER TABLE article_tag
+ADD CONSTRAINT FK_article_tag_article
+FOREIGN KEY (article) REFERENCES article(id_article)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+ALTER TABLE article_tag
+ADD CONSTRAINT FK_article_tag_tag
+FOREIGN KEY (tag) REFERENCES tag(id_tag)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+CREATE TABLE user_tag (
+  user INT NOT NULL,
+  tag INT NOT NULL
+);
+
+ALTER TABLE user_tag
+ADD CONSTRAINT FK_user_tag_article
+FOREIGN KEY (user) REFERENCES user(id_user)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+ALTER TABLE user_tag
+ADD CONSTRAINT FK_user_tag_tag
+FOREIGN KEY (tag) REFERENCES tag(id_tag)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
